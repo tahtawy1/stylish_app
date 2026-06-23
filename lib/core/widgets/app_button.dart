141277978cf2040f164512bdeea239ab0dcabe1a
Taha Sabry <sabrytaha854@gmail.com>
2026-06-24 00:00:06 +0300

    create: App button

---
+++ lib/core/widgets/app_button.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:stylish_app/core/extensions/color_scheme_ext.dart';
+import 'package:stylish_app/core/extensions/text_theme_ext.dart';
+
+class AppButton extends StatelessWidget {
+  const AppButton({
+    super.key,
+    required this.title,
+    this.postfixIcon,
+    this.enabled = true,
+  });
+
+  final String title;
+  final IconData? postfixIcon;
+  final bool enabled;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 54,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: enabled ? context.colors.primary : context.colors.outline,
+        borderRadius: BorderRadius.circular(32),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            title,
+            style: context.textStyle.bodyMedium?.copyWith(
+              color: context.colors.onPrimary,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          if (postfixIcon != null)
+            Padding(
+              padding: const EdgeInsetsDirectional.only(start: 8),
+              child: Icon(
+                postfixIcon,
+                color: context.colors.onPrimary,
+                size: 24,
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
