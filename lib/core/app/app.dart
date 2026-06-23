f68c6ad8cb0713a2447efc6ced4ddde8b5260b06
Taha Sabry <sabrytaha854@gmail.com>
2026-06-23 21:53:11 +0300

    chore: setup Firebase

--- lib/core/app/app.dart
+++ lib/core/app/app.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:stylish_app/core/localization/generated/l10n.dart';
 import 'package:stylish_app/core/router/app_router.dart';
 import 'package:stylish_app/core/theme/app_theme.dart';
-import 'package:stylish_app/core/localization/generated/l10n.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
+    return MaterialApp(
+      // routerConfig: AppRouter.router,
       debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      locale: const Locale('ar'),
+      locale: const Locale('en'),
       localizationsDelegates: const [
         S.delegate,
         GlobalMaterialLocalizations.delegate,
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:stylish_app/core/app/app.dart';
+import 'package:stylish_app/core/app/app_initializer.dart';
 
-void main() {
+void main() async {
+  await AppInitializer().initApp();
   runApp(
-    DevicePreview(enabled: !kReleaseMode, builder: (context) => const MyApp()),
+    DevicePreview(enabled: !kReleaseMode, builder: (context) => const App()),
   );
 }
